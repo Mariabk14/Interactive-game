0bbebe63f8fab66b8b0378e337fe0e5295ec23b4
Mariabk14 <157731218+Mariabk14@users.noreply.github.com>
2026-05-09 00:11:36 +0200

    css

--- src/js/choice.js
+++ src/js/choice.js
@@ -11,17 +11,17 @@ const form = document.createElement("form")
 form.classList.add('form-choise')
 const  numberOne = document.createElement("input");
 numberOne.classList.add('nummber-choice')
-numberOne.setAttribute('type', 'number');
+numberOne.setAttribute('type', 'text');
 numberOne.setAttribute('placeholder', 'Введіть число');
 
 const numberTwo = document.createElement('input');
 numberTwo.classList.add('nummber-choice');
-numberTwo.setAttribute('type', 'number');
+numberTwo.setAttribute('type', 'text');
 numberTwo.setAttribute('placeholder', 'Введіть число');
 
 const numberThree = document.createElement('input');
 numberThree.classList.add('nummber-choice');
-numberThree.setAttribute('type', 'number');
+numberThree.setAttribute('type', 'text');
 numberThree.setAttribute('placeholder', 'Введіть число');
 
 const numberText = document.createElement("p")
@@ -36,39 +36,17 @@ form.appendChild(numberThree);
 choiceContainer.appendChild(numberText)
 
 const refs = {
-    input : document.querySelectorAll(".nummber-choice")
+  inputs: document.querySelectorAll(".nummber-choice"),
+  result: document.querySelector(".number-text-choice")
 }
-console.log(refs.input)
-let numbers =[]
-refs.input.forEach(input => {
-    // const value = Number(input.value)
-    console.log(input.value)
-    // numbers.push(value)
-    // return numbers
+
+function updaitNumberArrai() {
+  let numbers =[]
+refs.inputs.forEach(input => {
+    const value = input.value
+    numbers.push(value)
 })
-console.log(numbers)
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-    const num1 = numberOne.value;
-  const num2 = numberTwo.value;
-  const num3 = numberThree.value;
-
-  if (num1 === '' || num2 === '' || num3 === '') {
-      numberText.textContent = 'Введіть всі три числа!';
-    return;
-  }
-
-  const n1 = Number(num1);
-  const n2 = Number(num2);
-  const n3 = Number(num3);
-
-  if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
-    numberText.textContent = 'Введені дані не є числами!';
-    return;
-  }
-
-  const max = Math.max(n1, n2, n3);
-
-  numberText.textContent = `Найбільше число, яке ви ввели - ${max}`;
-});
+  const maxNumber = Math.max(...numbers)
+  refs.result.textContent = `Найбільше число, яке ви ввели - ${maxNumber}`;
+}
+refs.inputs.forEach(input => (input.addEventListener("input", updaitNumberArrai)))
